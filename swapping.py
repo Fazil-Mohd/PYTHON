f078367cb98c87684e8ede182d8f4f03fc29e007
fazilmca <fazilmca1@gmail.com>
2024-10-01 12:27:00 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,9 @@
+x = int(input("Enter the Number "))
+y = int(input("Enter the Number "))
+
+print("x : ", x, " and y : ", y)
+
+print ("AFTER SWAPPING")
+
+x , y = y ,x
+print("x : ", x, " and y : ", y)
