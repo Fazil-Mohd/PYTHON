ee61ae431b84396d9c20b30ab13676677b1998a7
fazilmca <fazilmca1@gmail.com>
2024-10-01 12:16:32 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,12 @@
+print("Enter marks of five subjects: ")
+S1=float(input("DFCA:"))
+S2=float(input("PYTHON"))
+S3=float(input("MATHS"))
+S4=float(input("DS"))
+S5=float(input("C PROGRAMMING"))
+
+total = S1 + S2 + S3 + S4 + S5;
+print ("total = ",total)
+percentage = (total / 500) * 100;
+
+print ("Percentage = ",percentage)
