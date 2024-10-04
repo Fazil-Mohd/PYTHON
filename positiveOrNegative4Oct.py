0acd49c36e36f66d97e506cc601f00203788620d
fazilmca <fazilmca1@gmail.com>
2024-10-04 12:30:57 +0530

    Delete positiveOrNegative4Oct.py

--- positiveOrNegative4Oct.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a number "))
-if (n>=0):
-    if (n==0):
-        print ("Number is positive")
-    else:
-        print ("Number is positive")
-else:
-    print("Number is Negative")
