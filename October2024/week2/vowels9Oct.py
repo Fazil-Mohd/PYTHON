259d7442b4e02240cc333a02640f31d66e44b6aa
fazilmca <fazilmca1@gmail.com>
2024-10-09 11:36:49 +0530

    vowels9Oct.py

--- October2024/week2/vowels9Oct.py
+++ October2024/week2/vowels9Oct.py
@@ -1,9 +1,10 @@
 n=input("Enter The String")
 
 vowels_count=0
-vowels = ['a','e','i','o','u']
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+
 for char in n:
     if char in vowels:
         vowels_count+=1
-print("vowelss=",vowels_count)
-    
+        print(char)
+print("vowels=",vowels_count)    
