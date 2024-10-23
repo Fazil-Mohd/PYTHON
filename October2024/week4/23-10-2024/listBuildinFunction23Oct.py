2e4fa52ccf50cd88c2cec5cc7ff8b9f1aa8273e5
fazilmca <fazilmca1@gmail.com>
2024-10-23 12:34:39 +0530

    Update listBuildinFunction23Oct.py

--- October2024/week4/23-10-2024/listBuildinFunction23Oct.py
+++ October2024/week4/23-10-2024/listBuildinFunction23Oct.py
@@ -1,19 +1,14 @@
 my_list= list(map(int,input("Enter The Elemets Two the List Sepate it by space").split()))
 
 minimum = min(my_list)
-maximum = max(my_list)
-
-count_num = int(input("Enter The Number To Count"))
-count = my_list.count(count_num)
-
-total = sum(my_list)
-my_list.reverse()
-
-my_list.sort()
-
 print("Minimum value:", minimum)
+maximum = max(my_list)
 print("Maximum value:", maximum)
-print("Count of ",count_num,":", count)
+count=len(my_list)
+print("Count of list:", count)
+total = sum(my_list)
 print("Total sum:", total)
-print("Reversed list:", my_list)
+my_list.sort()
 print("Sorted list:", my_list)
+my_list.reverse()
+print("Reversed list:", my_list)
