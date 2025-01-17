b8587f698528853054a56ee7ada6e3b0fe27c9ec
fazilmca <fazilmca1@gmail.com>
2025-01-17 22:42:13 +0530

    Update 1-LeepYear4Oct.py

--- RECORD/1-LeepYear4Oct.py
+++ RECORD/1-LeepYear4Oct.py
@@ -3,7 +3,7 @@ current_year = int(input("Enter Current Year: "))
 final_year = int (input("Enter Final Year: "))
 
 for year in range(current_year ,final_year +1):
-    if year %4 == 0 :
+    if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
         leap_year.append(year)
         
 print("leap years :\n",leap_year)
