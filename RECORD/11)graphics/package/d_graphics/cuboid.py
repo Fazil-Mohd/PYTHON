7b646871349025362bb62227feb0b5d22b285a73
fazilmca <fazilmca1@gmail.com>
2025-01-19 12:26:32 +0530

    Delete RECORD/11)graphics directory

--- RECORD/11)graphics/package/d_graphics/cuboid.py
+++
@@ -1,6 +0,0 @@
-def cuboid_area(length,breadth,height):
-        return 2 *( length * breadth + breadth + height* length)
-
-def cuboid_perimeter(length,breadth,height):
-        return 4 * (length + breadth + height)
-        
